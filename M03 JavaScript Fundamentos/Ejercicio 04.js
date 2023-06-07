c852574ba7801c85bdf5923083bdf6b5058bae9b
jairo vence mattias <jairo_vencemattias@hotmail.com>
2023-06-07 02:44:31 -0300

    termine con los Ej.4

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,45 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   var numero = Math.pow(num, 2);
+   return numero;
+
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   var portres = Math.pow(num, 3);
+   return portres;
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   var expo = Math.pow(num, exponent);
+   return expo;
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var masprox = Math.round(num);
+   return masprox;
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   var arriba = Math.ceil(num);
+   return arriba;
+
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var rdm = Math.random(0, 1);
+   return rdm;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
